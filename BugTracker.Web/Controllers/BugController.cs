4b0ff0f20f54545cb0385573919804c5fe8e9706
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2019-06-17 18:12:17 +0100

    scaffold bug and person

---
+++ BugTracker.Web/Controllers/BugController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BugTracker.Web.Models;
+
+namespace BugTracker.Web.Controllers
+{
+    public class BugController : Controller
+    {
+        private readonly BugTrackerDbContext _context;
+
+        public BugController(BugTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bug
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bugs.ToListAsync());
+        }
+
+        // GET: Bug/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bugs
+                .FirstOrDefaultAsync(m => m.BugId == id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+
+            return View(bug);
+        }
+
+        // GET: Bug/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bug/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BugId,Created,Title,Description,IsClosed")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bug);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bug);
+        }
+
+        // GET: Bug/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bugs.FindAsync(id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+            return View(bug);
+        }
+
+        // POST: Bug/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BugId,Created,Title,Description,IsClosed")] Bug bug)
+        {
+            if (id != bug.BugId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bug);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BugExists(bug.BugId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bug);
+        }
+
+        // GET: Bug/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bugs
+                .FirstOrDefaultAsync(m => m.BugId == id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+
+            return View(bug);
+        }
+
+        // POST: Bug/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bug = await _context.Bugs.FindAsync(id);
+            _context.Bugs.Remove(bug);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BugExists(int id)
+        {
+            return _context.Bugs.Any(e => e.BugId == id);
+        }
+    }
+}
